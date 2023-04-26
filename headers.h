3593bf289112a67550d7aed988d07da2f75ec82f
MatiasLarrosa <matularrosa555@gmail.com>
2023-04-26 15:36:38 -0300

    agregado headers

---
+++ headers.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include <iostream>
+#include <cstdlib>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+enum eArtBanyo { barralesCortina, cepilloLimpieza, virulana, lanaMetal };
+enum eArtBazar { tenderesRopa, tablasPlanchar, sogas, tablasInodoro, impBanyera };
+enum eArtCerraje { llaveSimple, laveDobleTambor, llaveCod, llaveMagne };
+enum eArtElect { cables, lamparas, portalamparas, enchufes };
+enum eArtFerre { clavos, tarugos, tornillos, caños, flexibles, tapones };
+enum eHerramCaras { amoladora, lijadora, perforadora };
+enum eTiposPago { tarjetaDebito, mercadoPago, efectivo, tarjetaCredito };
