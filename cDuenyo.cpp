88e40159578a90c7e529a6f2f812e183674094d9
MatiasLarrosa <matularrosa555@gmail.com>
2023-05-14 12:17:33 -0300

    Cambios a Duenyo

--- cDuenyo.cpp
+++ cDuenyo.cpp
@@ -1,12 +1,34 @@
 #include "cDuenyo.h"
 
-cDuenyo::cDuenyo(double sueldo, const string Dni, const string Name, string Tel, string Email, string Adress) : cEmpleado(sueldo,Dni,Name,Tel,Email,Adress) {
+cDuenyo::cDuenyo(double sueldo, const string Dni, const string Name, string Tel, string Email, string Adress) : cEmpleado(sueldo, Dni, Name, Tel, Email, Adress) {
 }
 
 cDuenyo::~cDuenyo() {
 }
 
-void cDuenyo::cobrarPago() {
+void cDuenyo::archivarRecibo(cRecibo*) {
 
+}
+
+void cDuenyo::atenderCliente(cCliente*) {
+
+}
+
+//Dueño cumple la funcion de cajero y realiza en tiempo de ejecucion
+float cDuenyo::cobrarPago(vector <cProducto*> listaCompras)
+{
+	if (listaCompras.empty())//no cobro monto nulo	
+		return;
+
+	float montoTotal = 0.0;
+	for (int i = 0; i < listaCompras.size(); i++)
+	{
+		montoTotal += listaCompras[i]->getPrecio();
+	}
+	return montoTotal; //monto será sumado a fondos de ferreteria y restado a fondos de cliente
+}
+
+//tres opciones. metodo de pago online. no hay vuelto. dinero? entonces vuelvo.
+void cDuenyo::calcularVuelto(float montoCompra) {
 }
 
--- cDuenyo.h
+++ cDuenyo.h
@@ -1,4 +1,3 @@
-
 #ifndef _CDUENYO_H
 #define _CDUENYO_H
 
@@ -6,13 +5,15 @@
 #include "cCliente.h"
 #include "cRecibo.h"
 
-class cDuenyo: public cEmpleado {
+class cDuenyo : public cEmpleado {
 public:
-    cDuenyo(double sueldo, const string Dni, const string Name, string Tel, string Email, string Adress);
-    ~cDuenyo();
+	cDuenyo(double sueldo, const string Dni, const string Name, string Tel, string Email, string Adress);
+	~cDuenyo();
 
-    void generarRecibo(cRecibo);
-    void atenderCliente(cCliente);
+	void archivarRecibo(cRecibo*);
+	void atenderCliente(cCliente*);
+	float cobrarPago(vector <cProducto*> listaCompras);
+	void calcularVuelto(float montoCompra);
 };
 
 #endif //_CDUENYO_H
